f4cfc66a379490440f10a8c746222e671f835099
Cruzzel <j105005000rK>
2019-08-15 20:32:16 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
 /*Slider*/
 
-$('.multiple-items').slick({
+ $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
@@ -33,5 +33,47 @@ $('.multiple-items').slick({
   dots: true,
   dotsClass: 'dots-style',
   prevArrow: false,
-  nextArrow: false
+  nextArrow: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    }]
+ });
+/*Меню*/
+$('ul.menu a[href^="#"').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 500);
+    $('ul.menu a[href^="#"').css({'color':'#212121'});
+    $(this).css({'color':'#004bee'});
+    return false;
+});
+/*Выпадающее меню*/
+$('.menu-icon').click(function() {
+    $('nav').slideToggle(500);
+    $('ul').css({
+        'display':'flex', 'flex-direction':'column'
+    })
+    if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+        $(this).html('<i class="fas fa-times"></i>')
+    } else {
+        $(this).html('<i class="fas fa-bars"></i>')
+    }
+})
+/*Кнопка наверх*/
+$(window).scroll(function () {
+    if ($(this).scrollTop() !=0)
+        $('#toTop').fadeIn();
+    else
+        $('#toTop').fadeOut();
+});
+$('#toTop').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
 });
